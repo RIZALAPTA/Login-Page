da3f6215c424e146a4b3594573f8a288f823a237
RIZALAPTA <rizalapta88@gmail.com>
2024-08-13 07:33:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginButton = document.getElementById('loginButton');
+
+    loginButton.addEventListener('click', () => {
+        window.location.href = 'https://rizallapta.github.io/MyPortofolio/';
+    });
+});
